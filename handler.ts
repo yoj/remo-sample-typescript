bcbabf3cad48670853011badd98865fe227b6c78
山浦 洋司 <yoji_yamaura@mdmb01.local>
2020-05-13 10:22:01 +0900

    bug fix

--- handler.ts
+++ handler.ts
@@ -9,33 +9,34 @@ const ENDPOINTT_TO_GET = "https://api.nature.global/1/devices";
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
 
-  var body;
-  (async () => {
-    try {
-      const result = await got.get(ENDPOINTT_TO_GET, {
-        //body: jsonDataImported,
-        //json: true,
-        headers: {
-          'Content-Type': "application/json;charset=utf-8",
-          Authorization: `Bearer ${ACCESS_TOKEN}`
-        }
-      });
+  //(async () => {
+  try {
+    //const result = await got.get(ENDPOINTT_TO_GET, {
+    const result = await got.get(ENDPOINTT_TO_GET, {
+      //body: jsonDataImported,
+      //json: true,
+      headers: {
+        'Content-Type': "application/json;charset=utf-8",
+        Authorization: `Bearer ${ACCESS_TOKEN}`
+      }
+    });
 
-      body = result;
-      
-    } catch(e) {
-      console.log(`ERROR:\n${JSON.stringify(e)}`);
-    }
-  })();
+    console.log(result.body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      result: body,
-    }, null, 2),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: result.body,
+      }, null, 2),
+    };
+
+  } catch(e) {
+    console.log("error!!!!");
+
+    return {
+      statusCode: 500,
+      body: "error"
+    };
+  }
+  //})();
 }
-
-
-curl -X GET "https://api.nature.global/1/devices" -H "accept: application/json" -k --header "Authorization: Bearer Nv1FdHjRGh2hOeJW7QmckwbvlBSuc1liNelt2Y7_XLk.f6w9a6A1-9jaR-dMH_LToKg42Pf23ko_JaEOEh3Pikc"
